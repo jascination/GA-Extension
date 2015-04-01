9945f40b9393fcb493d5f20ce063376e428455fa
James <james@agoodman.com.au>
2015-04-01 22:32:40 +1100

    FIX

--- edms.js
+++ edms.js
@@ -105,7 +105,7 @@ $(document).ready(function(){
 		title: title,
 		utm: 'http://generalassemb.ly/education?where=' + metro + utm
 	}, function (result) {
-		$(".column.purchase-module").prepend('<div class="actions" style="margin-bottom: 1rem;"><a href="http://www.ancient.cool/collect?auto=true" target="_blank" class="button fluid js-purchase-attend">Auto-Collect Checkins</a></div>')
+		$(".column.purchase-module").prepend('<div class="actions" style="margin-bottom: 1rem;"><a href="http://www.ancient.cool/collect-setup?auto=true" target="_blank" class="button fluid js-purchase-attend">Auto-Collect Checkins</a></div>')
 	    
 		console.log("Variables stored for Ancient Cool");
 	});
