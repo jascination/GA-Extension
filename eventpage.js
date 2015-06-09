443a72f704c050af31273acc9d1686b7d83bdbec
jascination <james@Jamess-MacBook-Air.local>
2015-06-09 17:55:10 +1000

    Chng

--- eventpage.js
+++ eventpage.js
@@ -7,6 +7,8 @@ $(document).ready(function(){
 		var ref = $(this).attr('href'); 
 		arr.push(ref.split('/')[4]);
 	}); 
+
+
 	for (var i = 0; i<arr.length; i++){
 		if(arr[i]){
 			$('.date-topic-container').eq(i).append('<div class="actions"><a style="margin-bottom: 1rem; padding:1rem;" href="https://generalassemb.ly/admin/instances/' + arr[i] + '" class="button fluid js-purchase-attend">Go to Instance Page</a></div>');
